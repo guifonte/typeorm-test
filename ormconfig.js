3c35ead863513f66317e41a8f1473ba2ef637c0d
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-18 21:35:45 -0300

    change ormconfig to prod

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
+export default {
    "type": "mysql",
-   "host": "localhost",
-   "port": 3306,
-   "username": "root",
-   "password": "test",
-   "database": "test",
+   "host": process.env.RDS_HOSTNAME,
+   "port": process.env.RDS_PORT,
+   "username": process.env.RDS_USERNAME,
+   "password": process.env.RDS_PASSWORD,
+   "database": process.env.RDS_DB_NAME,
    "synchronize": true,
    "logging": false,
    "entities": [
